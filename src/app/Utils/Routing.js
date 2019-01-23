6b5fffc254b0fc925006d42bc94925f0fe974e14
Kubenic <jordan.venant@gmail.com>
2019-01-24 00:00:11 +0100

    Changing controller loading in routing - Dynamic import module not implemented in firefox

--- src/app/Utils/Routing.js
+++ src/app/Utils/Routing.js
@@ -1,7 +1,9 @@
+
 export default class Routing {
     constructor(routes){
         this.routes = routes;
         this.handleRoute();
+
     }
 
     handleRoute(){
@@ -9,8 +11,15 @@ export default class Routing {
             let rxps = this.prepareRxpString(route);
             let rxp = new RegExp("^"+rxps.pattern+"$");
             if(rxp.test(window.location.pathname)){
-                let Class = this.routes[route].split("@");
-                import("../Controller/"+Class[0]+".js")
+                /**
+                 * NEED MODIFICATION
+                 * import() only works on Chrome
+                 * dynamic import not validated yet
+                 * https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Instructions/import#Import_dynamique
+                 */
+
+                //let Class = this.routes[route].split("@");
+                /*import("../Controller/"+Class[0]+".js")
                     .then((module) =>{
                         let args = [];
                         if(rxps.args){
@@ -20,6 +29,15 @@ export default class Routing {
                         let controller = new  module.default();
                         controller[Class[1]].bind(controller).apply(null, args)
                     })
+                */
+                let args = [];
+                if(rxps.args){
+                    args = window.location.pathname.match(rxps.pattern);
+                    args.shift();
+                }
+
+                let controller = new this.routes[route].controller;
+                controller[this.routes[route].action].bind(controller).apply(null,args)
 
             }
         }
