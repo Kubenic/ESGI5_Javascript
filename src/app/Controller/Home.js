b7e70f8f7f7af4478fb2e04b64540a6f57209fdf
Kubenic <jordan.venant@gmail.com>
2019-01-24 09:37:23 +0100

    Starting a template system done

--- src/app/Controller/Home.js
+++ src/app/Controller/Home.js
@@ -1,10 +1,18 @@
+import APIHelper from '../Utils/APIHelper.js';
+
+import * as HTMLMarkup from '../Templates/Home.js';
+
 export default class Home{
     constructor(){
 
     }
     view(){
-        console.log("VIEW");
-        console.log(this);
+        APIHelper.discover(1)
+            .then((value) => {
+                //console.log(value);
+                //console.log(HTMLMarkup);
+                document.body.innerHTML = HTMLMarkup.generate('<h2>Header</h2>','<h2>Footer</h2>',['test','test','test','test']);
+            })
     }
 
 }
